50b66687525f834355586a27e5213f16f451646a
ernest <ernest@etcm.me>
2019-03-16 00:30:32 +0800

    Re-aligned cell layout

--- src/CountryCodeList.js
+++ src/CountryCodeList.js
@@ -144,7 +144,7 @@ const styles = StyleSheet.create({
   },
   cell: {
     paddingLeft: 20,
-    paddingRight: 31,
+    paddingRight: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
